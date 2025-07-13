513c6da8c34b192b6b7fb17e7f7f4058fb07c510
Mounika4546 <165478445+Mounika4546@users.noreply.github.com>
2025-07-13 14:58:10 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const contactRoute = require("./router/contact-router");
 const errorMiddleware = require("./middlewares/validate-middleware");
 
 const corsOptions = {
-origin: "http://localhost:5173",
+origin: ['http://localhost:5173', 'https://foodmanagement-1.onrender.com'],
 method: "GET,POST,PUT,DELETE,PATCH,HEAD",
 credentials:true,
 };

b502bdf8e6c0f9293a34410c831797a41c1bb2f0
Mounika4546 <165478445+Mounika4546@users.noreply.github.com>
2025-07-13 14:50:53 +0530

    Update Contact.jsx
    updated

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -23,7 +23,7 @@ export const Contact = () => {
     // console.log(contact);
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:5000/api/form/contact", {
+      const response = await fetch("https://foodmanagement-1a39.onrender.com/api/form/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
